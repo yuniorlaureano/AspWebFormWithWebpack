d5b8918c0aad44138eea642d80336ad3f906f549
Yunior Miguel Laureano <yu.laureano@dga.gov.do>
2018-11-27 08:56:39 -0400

    Configurando tareas y exclucion de archivos y carpetas. Utilizacion de gulp para tareas, csproj para excluir archivos

---
+++ AspWebFormWithWebpack/gulpfile.js
@@ -0,0 +1,29 @@
+﻿const gulp = require("gulp");
+const concat = require('gulp-concat');
+
+const path = {
+    nodeModules: "./node_modules/",
+    scriptDest: "./Scripts/",
+    styleDest: "./Content/"
+};
+
+gulp.task("copy:css", function () {
+
+    const cssToCopy = [
+        `${path.nodeModules}toastr/build/toastr.min.js`
+    ];
+
+    return gulp.src(cssToCopy).pipe(concat("vendor.css")).pipe(gulp.dest(`${path.styleDest}`));
+});
+
+
+gulp.task("copy:js", function () {
+
+    const jsToCopy = [
+        `${path.nodeModules}toastr/build/toastr.min.css`
+    ];
+
+    return gulp.src(jsToCopy).pipe(concat("vendor.js")).pipe(gulp.dest(`${path.scriptDest}`));
+});
+
+gulp.task("default", ['copy:css', 'copy:js']);
